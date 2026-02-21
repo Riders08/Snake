2eef3e68a8ecdd9991f9476ccee7439855595db7
Lucas Rivoal <lucas.rivoal@etu.u-bordeaux.fr>
2026-02-21 14:59:52 +0100

    ⚡️ Amélioration du serpent et de la map
    Maintenant la boucle et créer et on traite également le cas hors jeu avec la redirection du mur inverser

--- js/main.js
+++ js/main.js
@@ -1,14 +1,27 @@
-import { grow, changeDirection, move, snake } from "./snake";
+import { grow, changeDirection, move, snake } from "./snake.js";
+import { GRID_SIZE, TILE_SIZE } from "./data.js";
 const canvas = document.getElementById("game-canvas");
 const ctx = canvas.getContext("2d");
-
-export const GRID_SIZE = 20;
-const TILE_SIZE = 20;
 
 canvas.height = GRID_SIZE * TILE_SIZE;
 canvas.width = GRID_SIZE * TILE_SIZE;
 
-snake.forEach(element => {
-    ctx.fillStyle = "green",
-    ctx.fillRect(element.x * TILE_SIZE, element.y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
-});
+document.addEventListener("keydown", (e) =>{
+    console.log(e.key);
+    changeDirection(e.key);
+})
+
+function drawSnake(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    snake.forEach(element => {
+        ctx.fillStyle = "green",
+        ctx.fillRect(element.x * TILE_SIZE, element.y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+    });
+    ctx.fillStyle = "lightgreen",
+    ctx.fillRect(snake[0].x * TILE_SIZE, snake[0].y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+}
+
+setInterval(() =>{
+    move();
+    drawSnake();
+}, 500);
--- js/snake.js
+++ js/snake.js
@@ -1,6 +1,6 @@
-import { GRID_SIZE } from "./main";
+import { GRID_SIZE } from "./data.js";
 
-head = {
+let head = {
     x: getRandomInt(GRID_SIZE/4,GRID_SIZE-5), 
     y: getRandomInt(GRID_SIZE/4,GRID_SIZE-5)
 }
@@ -21,6 +21,15 @@ export function move(){
         x: snake[0].x + direction.x,
         y: snake[0].y + direction.y
     }
+    if(newHead.x < 0){
+        newHead.x = GRID_SIZE-1;
+    }else if(newHead.x >= GRID_SIZE){
+        newHead.x = 0;
+    }else if(newHead.y < 0){
+        newHead.y = GRID_SIZE-1;
+    }else if(newHead.y >= GRID_SIZE){
+        newHead.y = 0;
+    }
     snake.unshift(newHead);
     snake.pop();
 }
@@ -38,18 +47,31 @@ function getRandomInt(min, max){
 }
 
 export function changeDirection(key){
+    let actual_direction = direction;
     switch(key){
         case "ArrowLeft":
+            if(actual_direction.x == 1 &&  actual_direction.y == 0){
+                break;
+            }
             direction = {x: -1, y: 0};
             break;
         case "ArrowRight":
+            if(actual_direction.x == -1 &&  actual_direction.y == 0){
+                break;
+            }
             direction = {x: 1, y: 0};
             break;
         case "ArrowUp":
-            direction = {x: 0, y: 1};
+            if(actual_direction.x == 0 &&  actual_direction.y == 1){
+                break;
+            }
+            direction = {x: 0, y: -1};
             break;
         case "ArrowDown":
-            direction = {x: 0, y: -1};
+            if(actual_direction.x == 0 &&  actual_direction.y == -1){
+                break;
+            }
+            direction = {x: 0, y: 1};
             break;
         default: 
             direction; 
